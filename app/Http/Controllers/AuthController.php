f852f45e5101a575e94d2c1b459af1d9ed2fcbfe
lucas2726 <lucasbueno722g@gmail.com>
2025-03-03 16:14:03 -0400

    base de dados

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller {
 
@@ -33,6 +34,13 @@ class AuthController extends Controller {
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
+        // teste database connection 
+        try {
+            DB::connection()->getPdo();
+            echo "okkkkkkkkkk"; 
+        } catch (\PDOException $e) {
+            echo "jsjsjs" . $e->getMessage();
+        }
         echo "jhhhh";
     }
 
